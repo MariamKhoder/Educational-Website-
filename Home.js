66806bec8894578426ea606e16f31c340a0fdbf9
MariamKhoder <redamariam338@gmail.com>
2025-05-22 19:08:56 +0300

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,10 @@
+
+function openSidebar() {
+    document.getElementById("mySidebar").style.width = "250px";
+  }
+function closeSidebar() {
+    document.getElementById("mySidebar").style.width = "0";
+  }
+  function goto() {
+    window.location.href="../Sign-page/sign.html";
+  }
